49a2ba7f320053ae6aa4151a7de39e8c7481d92f
p2moo <101615665+p2moo@users.noreply.github.com>
2023-05-03 18:52:28 +0800

    beginning to write smart contract

--- programs/simple_vesting_contract/src/lib.rs
+++ programs/simple_vesting_contract/src/lib.rs
@@ -9,7 +9,30 @@ pub mod simple_vesting_contract {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+  
+  	pub fn create_vesting()
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct VestingData {
+  current_amount: u64,
+  end_datetime: u64,
+  last_action_datetime: u64
+}
+
+#[derive(Accounts)]
+pub struct CreateVesting<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // space: 8 discriminator + 8 current_amount + 8 end_datetime + 8 last_action_datetime
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8 + 8 + 8, seeds = [b"vesting-data", user.key().as_ref()], bump
+    )]
+    pub vesting_data: Account<'info, VestingData>,
+    pub system_program: Program<'info, System>,
+}
